8d2af2fabaeeb3327f95c315dbab7ff180022baa
Beyonder12 <fajri.illahi1211@gmail.com>
2022-04-16 20:40:55 +0700

    transfer api

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"account-service-go-linkaja/db"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -16,6 +17,12 @@ type Accounts struct {
 	Balance       int    `json:"balance"`
 }
 
+//struct dto for trasfer
+type TransferDto struct {
+	ToAccountNumber int `json:"to_account_number"`
+	Amount          int `json:"amount"`
+}
+
 //receiver function
 func (Accounts) TableName() string {
 	return "accounts"
@@ -24,7 +31,7 @@ func (Accounts) TableName() string {
 func main() {
 	route := echo.New()
 	db := db.Init()
-	route.POST("account/create_account", func(c echo.Context) error {
+	route.POST("account/", func(c echo.Context) error {
 		account := new(Accounts)
 		c.Bind(account)
 
@@ -44,7 +51,7 @@ func main() {
 		return c.JSON(http.StatusOK, response)
 	})
 
-	route.GET("account/get_accounts", func(c echo.Context) error {
+	route.GET("account/", func(c echo.Context) error {
 
 		var accounts []Accounts
 		db.Find(&accounts)
@@ -60,5 +67,79 @@ func main() {
 		return c.JSON(http.StatusOK, response)
 	})
 
+	route.GET("account/:account_number", func(c echo.Context) error {
+		account := new(Accounts)
+		account.AccountNumber, _ = strconv.Atoi(c.Param("account_number"))
+
+		err := db.Where("account_number", account.AccountNumber).First(&account).Error
+		if err != nil {
+			fmt.Println("account number not found")
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		response := struct {
+			Message string
+			Data    Accounts
+		}{
+			Message: "Succesfully found an account",
+			Data:    *account,
+		}
+		return c.JSON(http.StatusOK, response)
+	})
+
+	route.POST("account/:from_account_number/transfer", func(c echo.Context) error {
+		//get fromaccount from param respectively
+		fromAccount := new(Accounts)
+		fromAccount.AccountNumber, _ = strconv.Atoi(c.Param("from_account_number"))
+
+		errFrom := db.Where("account_number = ?", fromAccount.AccountNumber).First(&fromAccount).Error
+		if errFrom != nil {
+			fmt.Println("from account number is not found")
+			return c.JSON(http.StatusNotFound, errFrom)
+		}
+
+		// get account using body request toAccountNumber
+		transferDto := new(TransferDto)
+		c.Bind(transferDto)
+
+		var toAccount Accounts
+
+		errTo := db.Where("account_number = ?", transferDto.ToAccountNumber).Find(&toAccount).Error
+		if errFrom != nil {
+			fmt.Println("error find account")
+			return c.JSON(http.StatusNotFound, errTo)
+		}
+
+		fromAccount.Balance = fromAccount.Balance - transferDto.Amount
+
+		errFrom = db.Save(fromAccount).Error
+		if errFrom != nil {
+			fmt.Println("error save from account")
+			return c.JSON(http.StatusBadRequest, errFrom)
+		}
+
+		toAccount.Balance = toAccount.Balance + transferDto.Amount
+
+		errTo = db.Save(toAccount).Error
+		if errTo != nil {
+			fmt.Println("error save to account")
+			return c.JSON(http.StatusBadRequest, errTo)
+		}
+
+		// update balance
+		response := struct {
+			Message     string
+			FromAccount Accounts
+			ToAccount   Accounts
+		}{
+			Message:     "Transaction success",
+			FromAccount: *fromAccount,
+			ToAccount:   toAccount,
+		}
+
+		return c.JSON(http.StatusAccepted, response)
+
+	})
+
 	route.Start(":9999")
+
 }
