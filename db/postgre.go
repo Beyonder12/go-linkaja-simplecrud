94f6fe7a16c9f7c4dd490f156e68bc2db11ba017
Beyonder12 <fajri.illahi1211@gmail.com>
2022-04-18 10:19:53 +0700

    docker

--- db/postgre.go
+++ db/postgre.go
@@ -15,7 +15,7 @@ func Init() *gorm.DB {
 }
 
 func getSession() *gorm.DB {
-	DB, error := gorm.Open(postgres.Open(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", "localhost", "5432", "postgres", "postgres", "12345678")), &gorm.Config{
+	DB, error := gorm.Open(postgres.Open(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", "database", "5432", "postgres", "postgres", "postgres")), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
---
+++ info.md
@@ -0,0 +1,5 @@
+
+docker build .
+docker compose build
+docker compose up
+docker compose down
